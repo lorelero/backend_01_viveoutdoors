2802c4435ac7b55f6f6d7492f89fc41ba4e09578
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-11-16 12:28:07 -0300

    actualizacion index.js

--- backend/index.js
+++ backend/index.js
@@ -1,174 +1,142 @@
-// npm init --yes
-// npm i express
-// npm i express-fileupload
-// npm i pg
-// npm i jsonwebtoken
-// npm i nodemon -D
-// npm i morgan
-// npm i dotenv
-// crear .env
-// crear .gitignore
-// abrir psql y crear database y table
+require('dotenv').config(); 
 
-// Importaciones
-//const expressFileUpload = require("express-fileupload");
-//const secretKey = "blog";
-// const dotenv = require("dotenv");
-//dotenv.config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const helmet = require('helmet');
+const { PORT, SECRET_JWT_KEY } = process.env; 
+const { 
+    registrarUsuario, 
+    iniciarSesion, 
+    cerrarSesion, 
+    accesoProtegido, 
+    pool 
+} = require('./consultas/consultasUsuarios.js');
+const { 
+    leerPublicaciones, 
+    insertarProducto, 
+    insertarImagenProducto, 
+    insertarPublicacion 
+} = require('./consultas/consultas.js');
+const { body, validationResult } = require('express-validator');
 
-// Importamos las dependencias necesarias para nuestra aplicación
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const {
-  leerPublicaciones,
-  insertarProducto,
-  insertarPublicacion,
-  insertarImagenProducto,
-  getProductos,
-  getProductoById,
-  getProductosSale,
-  getProductosCategorias,
-} = require("./consultas/consultas.js");
-require("dotenv").config(); // Cargamos las variables de entorno desde el archivo .env
-
-// Creamos una instancia de Express
 const app = express();
 
-// Configuramos el puerto en el que escuchará nuestra aplicación
-const PORT = process.env.PORT_SERVER || 3000;
+// Middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: SECRET_JWT_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: process.env.NODE_ENV === 'production' }
+}));
 
-// Usamos middleware para mejorar nuestra aplicación
-app.use(cors()); // Permite que nuestra API sea accesible desde diferentes orígenes
-app.use(morgan("dev")); // Registra las solicitudes en la consola para facilitar el desarrollo
-app.use(express.json()); // Permite que nuestra aplicación entienda el formato JSON en las solicitudes
+// Rutas
+app.post('/register', 
+    body('username').isString().notEmpty(),
+    body('password').isString().isLength({ min: 6 }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        registrarUsuario(req, res, next);
+    }
+);
 
-// Definimos nuestras rutas
-app.get("/", (req, res) => {
-  res.send("¡Hola, mundo! Bienvenido a nuestra aplicación."); // Respuesta amigable en la ruta raíz
+app.post('/login', 
+    body('username').isString().notEmpty(),
+    body('password').isString().isLength({ min: 6 }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        iniciarSesion(req, res, next);
+    }
+);
+
+app.post('/logout', cerrarSesion);
+app.get('/protected', accesoProtegido);
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
+// Ruta para obtener publicaciones
 app.get("/publicaciones", async (req, res) => {
-  const obtenerPublicaciones = await leerPublicaciones();
-  res.json({ obtenerPublicaciones });
+    try {
+        const obtenerPublicaciones = await leerPublicaciones();
+        res.json({ obtenerPublicaciones });
+    } catch (error) {
+        console.error('Error al obtener publicaciones:', error);
+        res.status(500).json({ error: 'Error al obtener publicaciones' });
+    }
 });
 
 // Ruta para crear un nuevo producto, imagen y publicación
-app.post("/crearpublicacion", async (req, res) => {
-  const {
-    nombre,
-    descripcion,
-    stock,
-    precio,
-    url,
-    texto_alternativo,
-    id_usuario,
-    estado,
-  } = req.body;
+app.post('/publicaciones', async (req, res) => {
+    const { nombre, descripcion, stock, precio, url_imagen, fecha_creacion, fecha_actualizacion, estado } = req.body;
 
-  try {
-    // Insertar el producto y obtener su ID
-    const nuevoProducto = await insertarProducto(
-      nombre,
-      descripcion,
-      stock,
-      precio
-    );
+    try {
+        const nuevoProducto = await insertarProducto(nombre, descripcion, stock, precio);
+        const nuevaImagen = await insertarImagenProducto(nuevoProducto.id_producto, url_imagen);
+        const nuevaPublicacion = await insertarPublicacion(nuevoProducto.id_producto, fecha_creacion, fecha_actualizacion, estado);
 
-    // Insertar la imagen del producto con el ID del nuevo producto
-    const nuevaImagen = await insertarImagenProducto(
-      nuevoProducto.id_producto,
-      url,
-      texto_alternativo
-    );
-
-    // Insertar la publicación con el ID del nuevo producto
-    const nuevaPublicacion = await insertarPublicacion(
-      nuevoProducto.id_producto,
-      id_usuario,
-      estado
-    );
-
-    res.status(201).json({
-      mensaje: "Publicación creada exitosamente",
-      producto: nuevoProducto,
-      imagen: nuevaImagen,
-      publicacion: nuevaPublicacion,
-    });
-  } catch (error) {
-    console.error("Error al crear la publicación:", error);
-    res.status(500).json({ error: "Error al crear la publicación" });
-  }
+        res.status(201).json({
+            mensaje: 'Publicación creada exitosamente',
+            producto: nuevoProducto,
+            imagen: nuevaImagen,
+            publicacion: nuevaPublicacion
+        });
+    } catch (error) {
+        console.error('Error al crear la publicación:', error);
+        res.status(500).json({ error: 'Error al crear la publicación' });
+    }
 });
 
-// Obtención de productos
-app.get("/productos", async (req, res) => {
-  try {
-    const productos = await getProductos();
-    res.json(productos);
-  } catch (error) {
-    res.status(error.code || 500).send(error);
-  }
+// Ruta para crear una publicación
+app.post('/crearpublicacion', async (req, res) => {
+    const { nombre, descripcion, stock, precio, url_imagen, texto_alternativo, id_usuario, estado } = req.body;
+
+    try {
+        // Insertar el producto y obtener su ID
+        const nuevoProducto = await insertarProducto(nombre, descripcion, stock, precio);
+
+        // Insertar la imagen del producto con el ID del nuevo producto
+        const nuevaImagen = await insertarImagenProducto(nuevoProducto.id_producto, url_imagen, texto_alternativo);
+
+        // Insertar la publicación con el ID del nuevo producto
+        const nuevaPublicacion = await insertarPublicacion(nuevoProducto.id_producto, id_usuario, estado);
+
+        res.status(201).json({
+            mensaje: 'Publicación creada exitosamente',
+            producto: nuevoProducto,
+            imagen: nuevaImagen,
+            publicacion: nuevaPublicacion
+        });
+
+    } catch (error) {
+        console.error('Error al crear la publicación:', error);
+        res.status(500).json({ error: 'Error al crear la publicación' });
+    }
 });
 
-// Obtención de productos por ID
-app.get("/productos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const producto = await getProductoById(id);
-    res.json(producto);
-  } catch (error) {
-    res
-      .status(error.code || 500)
-      .send(error.message || "Error interno del servidor");
-  }
-});
-
-// Obtención de productos por categorias
-app.get("/productos_categorias", async (req, res) => {
-  try {
-    const productos_categorias = await getProductosCategorias();
-    res.json(productos_categorias);
-  } catch (error) {
-    res.status(error.code || 500).send(error);
-  }
-});
-
-// Obtención de productos en Sale
-app.get("/productos_sale", async (req, res) => {
-  try {
-    const productos_sale = await getProductosSale();
-    res.json(productos_sale);
-  } catch (error) {
-    res.status(error.code || 500).send(error);
-  }
-});
-
-// Manejo de errores 404
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({ error: "Lo sentimos, recurso no encontrado. ¡Intenta otra vez!" });
+// Manejo de errores 404 
+app.use((req, res, next) => { 
+    res.status(404).json({ error: 'Lo sentimos, recurso no encontrado. ¡Intenta otra vez!' });
 });
 
 // Añadimos una ruta adicional para mostrar un saludo
-app.get("/api/saludo", (req, res) => {
-  res.json({
-    mensaje: "¡Bienvenido a la API! Esperamos que disfrutes tu experiencia.",
-  });
+app.get('/api/saludo', (req, res) => {
+    res.json({ mensaje: '¡Bienvenido a la API! Esperamos que disfrutes tu experiencia.' });
 });
 
-// Manejo de errores: si no encontramos la ruta, enviamos un mensaje amable
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .json({ error: "Lo sentimos, recurso no encontrado. ¡Intenta otra vez!" });
-});
-
-// Iniciamos el servidor y mostramos un mensaje para confirmar que está funcionando
+// Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(
-    `Servidor escuchando en http://localhost:${PORT}. ¡Gracias por usar nuestra aplicación!`
-  );
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
