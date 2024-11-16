f1d8cc4549db5b4537b413c0dec0e29660f1a2c9
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-11-16 14:59:45 -0300

    cambios en create

--- backend/consultas/user-repository.js
+++ backend/consultas/user-repository.js
@@ -11,22 +11,22 @@ const crypto = require('crypto');
 export class UserRepository {  
     static async create({ nombre, apellido, email, telefono, password, rol}) {
         // Verificar si el usuario ya existe
-        const existingUser = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        const existingUser = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
         if (existingUser.rows.length > 0) {
-            throw new Error('Username already exists');
+            throw new Error('El correo electrónico ya existe');
         }
 
         // Generar un ID único para el nuevo usuario
-        const id = crypto.randomUUID();
+       //  const id = crypto.randomUUID();   --> no es necesario, el id_usuario es SERIAL
         const hashedPassword = await bcrypt.hash(password, 10); // Puedes ajustar el número de saltos
 
         // Insertar el nuevo usuario en la base de datos
-        await pool.query('INSERT INTO users (id, username, password) VALUES ($1, $2, $3)', [id, username, hashedPassword]);
+        await pool.query('INSERT INTO usuarios (id_usuario, nombre, apellido, email, telefono, password, rol) VALUES (DEFAULT, $1, $2, $3, $4, $5, DEFAULT)', [nombre, apellido, email, telefono, hashedPassword]);
 
         return id;
     }
 
-    static async login({ username, password }) {
+    static async login({ email, password }) {
         // Buscar el usuario por nombre de usuario
         const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
         const user = result.rows[0];
