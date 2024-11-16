9ee08b887477cd1d5be979aae4b23287e2883b58
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-11-16 12:26:41 -0300

    consultas usuario

---
+++ backend/consultas/consultasUsuarios.js
@@ -0,0 +1,72 @@
+const pkg = require('pg');
+const { Pool } = pkg;
+const UserRepository = require('./user-repository.js');
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+const { SECRET_JWT_KEY } = require('../config.js');
+
+// Configuración del pool de conexiones a PostgreSQL
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: '3434',
+    database: 'viveoutdoors',
+    allowExitOnIdle: true
+});
+
+// Función para registrar un nuevo usuario
+const registrarUsuario = async (req, res) => {
+    const { username, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const id = await UserRepository.create({ username, password });
+        return res.status(201).json({ id });
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
+};
+
+// Función para iniciar sesión
+const iniciarSesion = async (req, res) => {
+    const { username, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await UserRepository.login({ username, password });
+        const token = jwt.sign(
+            { id: user._id, username: user.username },
+            SECRET_JWT_KEY,
+            { expiresIn: '1h' }
+        );
+
+        res
+            .cookie('access_token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'PRODUCTION',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60 // 1 hora
+            })
+            .json({ user, token });
+    } catch (error) {
+        console.error(error);
+        return res.status(401).send(error.message);
+    }
+};
+
+// Función para cerrar sesión
+const cerrarSesion = (req, res) => {
+    return res
+        .clearCookie('access_token')
+        .json({ message: 'Logout successful' });
+};
+
+// Función para acceder a una ruta
