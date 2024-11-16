5a2dba3a4c376e43efff7330798a6ab4c1174086
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-11-16 13:11:23 -0300

    eliminé ruta duplicada

--- backend/index.js
+++ backend/index.js
@@ -40,8 +40,9 @@ const { body, validationResult } = require('express-validator');
 
 // Middleware
 app.use(cors()); // Permite que nuestra API sea accesible desde diferentes orígenes
+app.use(morgan('dev')); // Registra las solicitudes en la consola para facilitar el desarrollo
 app.use(helmet());
-app.use(express.json());
+app.use(express.json());// Permite que nuestra aplicación entienda el formato JSON en las solicitudes
 app.use(cookieParser());
 app.use(session({
     secret: SECRET_JWT_KEY,
@@ -50,7 +51,9 @@ app.use(session({
     cookie: { secure: process.env.NODE_ENV === 'production' }
 }));
 
-// Rutas
+// Definimos nuestras rutas
+
+// RUTA POST PARA REGISTRO DE NUEVOS USUARIOS
 app.post('/register', 
     body('username').isString().notEmpty(),
     body('password').isString().isLength({ min: 6 }),
@@ -63,6 +66,7 @@ app.post('/register',
     }
 );
 
+//RUTA LOGIN PARA INGRESO DE USUARIOS YA REGISTRADOS
 app.post('/login', 
     body('username').isString().notEmpty(),
     body('password').isString().isLength({ min: 6 }),
@@ -84,7 +88,9 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-// Ruta para obtener publicaciones
+
+
+// Ruta para obtener publicaciones Y que se enlisten
 app.get("/publicaciones", async (req, res) => {
     try {
         const obtenerPublicaciones = await leerPublicaciones();
@@ -95,37 +101,17 @@ app.get("/publicaciones", async (req, res) => {
     }
 });
 
-// Ruta para crear un nuevo producto, imagen y publicación
-app.post('/publicaciones', async (req, res) => {
-    const { nombre, descripcion, stock, precio, url_imagen, fecha_creacion, fecha_actualizacion, estado } = req.body;
+// Ruta para crear una nueva publicación: la cual inserta un nuevo producto e imagen
 
-    try {
-        const nuevoProducto = await insertarProducto(nombre, descripcion, stock, precio);
-        const nuevaImagen = await insertarImagenProducto(nuevoProducto.id_producto, url_imagen);
-        const nuevaPublicacion = await insertarPublicacion(nuevoProducto.id_producto, fecha_creacion, fecha_actualizacion, estado);
-
-        res.status(201).json({
-            mensaje: 'Publicación creada exitosamente',
-            producto: nuevoProducto,
-            imagen: nuevaImagen,
-            publicacion: nuevaPublicacion
-        });
-    } catch (error) {
-        console.error('Error al crear la publicación:', error);
-        res.status(500).json({ error: 'Error al crear la publicación' });
-    }
-});
-
-// Ruta para crear una publicación
 app.post('/crearpublicacion', async (req, res) => {
-    const { nombre, descripcion, stock, precio, url_imagen, texto_alternativo, id_usuario, estado } = req.body;
+    const { nombre, descripcion, stock, precio, url, texto_alternativo, id_usuario, estado } = req.body;
 
     try {
         // Insertar el producto y obtener su ID
         const nuevoProducto = await insertarProducto(nombre, descripcion, stock, precio);
 
         // Insertar la imagen del producto con el ID del nuevo producto
-        const nuevaImagen = await insertarImagenProducto(nuevoProducto.id_producto, url_imagen, texto_alternativo);
+        const nuevaImagen = await insertarImagenProducto(nuevoProducto.id_producto, url, texto_alternativo);
 
         // Insertar la publicación con el ID del nuevo producto
         const nuevaPublicacion = await insertarPublicacion(nuevoProducto.id_producto, id_usuario, estado);
