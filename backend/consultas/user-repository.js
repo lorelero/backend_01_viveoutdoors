feca2366db64b434eed22dd696896d4208bcbec7
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-11-16 23:26:21 -0300

    cambio en el export de user-repository

--- backend/consultas/user-repository.js
+++ backend/consultas/user-repository.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcryptjs');
 
 // UserRepository verifica si el usuario existe, hashea la contraseña, genera un ID único e inserta el nuevo usuario en la base de datos
 class UserRepository {  
-    static async create({ nombre, apellido, email, telefono, password, rol}) {
+    static async create({ nombre, apellido, email, telefono, password}) {
         // Verificar si el usuario ya existe
         const existingUser = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
         if (existingUser.rows.length > 0) {
@@ -46,4 +46,4 @@ class UserRepository {
     }
 }
 
-module.exports = { UserRepository };
+module.exports =  UserRepository ;
