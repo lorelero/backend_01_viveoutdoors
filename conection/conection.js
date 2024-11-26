80699e7375f3db0a164c82dc01fdd8e79de2a755
Lorena Suarez <lorenandreasuarez@gmail.com>
2024-11-26 13:35:38 -0300

    actualizacion conection.js para render

--- conection/conection.js
+++ conection/conection.js
@@ -2,15 +2,16 @@
 //utilizando variables de entorno para la configuración y biblioteca pg
 
 const { Pool } = require('pg'); //importa la clase Pool de la biblioteca pg
+require ('dotenv').config();
 
-const { HOST, DATABASE, USER, PASSWORD, PORT } = process.env;  //extrae las variables de entorno necesarias para la conexión a la base de datos del archivo .env
+//const { HOST, DATABASE, USER, PASSWORD, PORT } = process.env;  //extrae las variables de entorno necesarias para la conexión a la base de datos del archivo .env
 
 const pool = new Pool({   //configuración de la conexión, se crea una instancia de Pool con la configuración necesaria para conectarse a la base de datos PostgreSQL.
-    host: HOST,
-    database: DATABASE,
-    user: USER,
-    password: PASSWORD,
-    port: PORT,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
     allowExitOnIdle: true,  // permite que la aplicación se cierre incluso si hay conexiones inactivas en el pool.
 });
 
