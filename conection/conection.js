b0ff18613d6ed3cbadf5eb1cefddd2253decfe80
Lorena Suarez <lorenandreasuarez@gmail.com>
2024-11-26 20:49:43 -0300

    actualizacion datos external url

--- conection/conection.js
+++ conection/conection.js
@@ -1,3 +1,22 @@
+// config localhost
+/* const { Pool } = require('pg'); //importa la clase Pool de la biblioteca pg
+
+const { HOST, DATABASE, USER, PASSWORD, PORT } = process.env;  //extrae las variables de entorno necesarias para la conexión a la base de datos del archivo .env
+
+const pool = new Pool({   //configuración de la conexión, se crea una instancia de Pool con la configuración necesaria para conectarse a la base de datos PostgreSQL.
+    host: HOST,
+    database: DATABASE,
+    user: USER,
+    password: PASSWORD,
+    port: PORT,
+    allowExitOnIdle: true,  // permite que la aplicación se cierre incluso si hay conexiones inactivas en el pool.
+});
+
+
+module.exports = { pool };
+ */
+
+//external url render
 const { Pool } = require('pg');
 
 const pool = new Pool({
